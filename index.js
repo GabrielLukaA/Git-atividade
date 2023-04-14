eafcb696bbb0df6363e94c11a7e4f2b55c2e5c44
GabrielLukaA <gabriel_luka-amaral@estudante.sesisenai.org.br>
2023-04-13 21:52:40 -0300

    atividade

---
+++ index.js
@@ -0,0 +1,26 @@
+var corpo = document.getElementsByClassName("meio");
+let i=0;
+function gerarCorDeterminada(){
+// <button class="botao" onclick="gerarCorDeterminada()">Simple</button>
+//<button class="botao" onclick="gerarCorHex()">Hex</button>
+}
+function gerarCorHex(){
+
+}
+function mudarCor(){
+    
+    if (i<5){
+        if (i==0){
+            document.body.style.backgroundColor="blue";
+        i++;           
+        }
+        if (i==1){
+document.body.style.backgroundColor="green";
+        i++;           
+        }
+        if (i==2){
+            document.body.style.backgroundColor="purple";
+        i++;           
+        }
+    }
+}
